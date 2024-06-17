2e87ce8fcb15b5c070ad44bfe6519bb7043c4ac8
Kapoline <polina.chit@mail.ru>
2024-06-17 11:28:01 +0300

    changed iou\ks methods

--- SD/rxd_ecs/ks_lfp.py
+++ SD/rxd_ecs/ks_lfp.py
@@ -1,13 +1,12 @@
 import numpy as np
-from scipy.stats import kstwo, ks_2samp, kstest
+from scipy.stats import ks_2samp
 import statistics as st
+import os
 
-def load_txt(filepath):
-    data = []
-    with open(filepath) as f:
-        for line in f:
-            data.append(float(line.split("\n")[0]))
-    return data
+def load_txt(file_path):
+    with open(file_path, 'r') as f:
+        data = [float(line.strip()) for line in f]
+    return np.array(data)
 
 def calculate_iou(arr1, arr2):
     startA = min(arr1)
@@ -24,33 +23,49 @@ def calculate_iou(arr1, arr2):
 
     return iou
 
-
-def main():
-    file = ""
-    matfile = ""
-
-    lfp_cc = load_txt(file)
-    lfp_mat = load_txt(matfile)
-
+def process_data(cc_file, mat_file, output_file):
+    lfp_cc = load_txt(cc_file)
+    lfp_mat = load_txt(mat_file)
 
     iou = calculate_iou(lfp_cc, lfp_mat)
-    print(f"IoU между массивом модели 1 и массивом модели 2: {iou}")
+    # print(f"IoU между массивом модели {os.path.basename(cc_file)} и массивом модели {os.path.basename(mat_file)}: {iou}")
 
-    #Kolmogorov-Smirnov test
-
-    subsample_size = 10000
+    # Kolmogorov-Smirnov test
+    subsample_size = min(10000, len(lfp_cc), len(lfp_mat))
     arr1 = np.random.choice(lfp_cc, size=subsample_size, replace=False)
     arr2 = np.random.choice(lfp_mat, size=subsample_size, replace=False)
-
     result = ks_2samp(arr1, arr2, method='asymp')
 
-    #Comparison of averages
+    # Comparison of averages
     mean_cc = st.mean(lfp_cc)
     mean_bio = st.mean(lfp_mat)
-    print(f"{result}")
-    print("Среднее значение в lfp_cc " + str(mean_cc))
-    print("Среднее значение в lfp_mat " + str(mean_bio))
+    # print(f"{result}")
+    # print(f"Среднее значение в {os.path.basename(cc_file)}: {mean_cc}")
+    # print(f"Среднее значение в {os.path.basename(mat_file)}: {mean_bio}")
 
+    # Запись результатов в файл
+    with open(output_file, "a") as f:
+        f.write(f"Файл 1: {os.path.basename(cc_file)}\n")
+        f.write(f"Файл 2: {os.path.basename(mat_file)}\n")
+        f.write(f"IoU: {iou}\n")
+        f.write(f"Kolmogorov-Smirnov test: {result}\n")
+        f.write(f"Среднее значение в {os.path.basename(cc_file)}: {mean_cc}\n")
+        f.write(f"Среднее значение в {os.path.basename(mat_file)}: {mean_bio}\n\n")
+
+def main():
+    cc_folder = ""
+    mat_folder = ""
+    output_file = ""
+
+    cc_files = [os.path.join(cc_folder, f) for f in os.listdir(cc_folder) if os.path.isfile(os.path.join(cc_folder, f))]
+    mat_files = [os.path.join(mat_folder, f) for f in os.listdir(mat_folder) if os.path.isfile(os.path.join(mat_folder, f))]
+
+    with open(output_file, "w") as f:
+        f.write("")
+
+    for cc_file in cc_files:
+        for mat_file in mat_files:
+            process_data(cc_file, mat_file, output_file)
 
 if __name__ == "__main__":
     main()
