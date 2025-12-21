bbebde12db8dec26e723b97f01e81b50f27c0746
renat-shk <renats@globalcloudteam.com>
2025-12-21 17:02:57 +0300

    attempt to fix 'AttributeError: 'nrn.Section' object has no attribute 'gbar_nmda''

--- fiber.py
+++ fiber.py
@@ -112,7 +112,7 @@ class fiber(object):
             self.soma.insert("cat")
             self.soma.insert("xiong")
             self.soma.gnabar_nachan = 0.4
-            self.soma(0.5).nachan.shiftm = -10
+            #self.soma.shiftm_nachan=-10 #self.soma(0.5).nachan.shiftm = -10 #
             self.soma.gnabar_nap = 0.000125
             self.soma.gkbar_ka = 0.2
             self.soma.gkbar_kdr = 0.2
@@ -140,7 +140,7 @@ class fiber(object):
                 sec.gcalbar_cal = 0.0001
                 sec.gcatbar_cat = 0.0001
                 sec.g_xiong = 0.0
-                sec.gbar_nmda = 0.0001
+                sec.gkbar_nmda = 0.0001
 
     def build_subsets(self):
         '''
